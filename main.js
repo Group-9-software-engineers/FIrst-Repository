607f5119f8dd8dac10b2ec7b4fecf7d065475de4
lijsamuael <habtamufentahunhfa@gmial.com>
2022-05-14 17:09:37 +0300

    modifing the js file

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
-var elm = document.getElementsByTagName("h1");
-var btn = document.getElementsByTagName('button')
+var btn = document.getElementById('btn');
+btn.addEventListener('click', 
+    function (){
+        document.getElementById('notify').innerHTML = 'You will find our github account in the new tab,\n just watch for the newlly added tab.';
+    }
+    );
 
-btn.onclick = function(){
-    alert('Button clicked');
-};
+ 
